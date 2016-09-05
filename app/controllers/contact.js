6cb08db6102cf68863be26f273afb8cd72eacd05
Kamilla <holanda.kamilla@gmail.com>
2016-09-05 18:40:44 -0300

    Saving email for invitation

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -16,8 +16,13 @@ export default Ember.Controller.extend({
 
     actions: {
       saveMessage(){
-          alert(`Saving message of the following email address is in progress: ${this.get('emailAddress')}`);
-          this.set('responseMessage', `We got your message and we’ll get in touch soon.`);
+          const email = this.get('emailAddress');
+
+          const newInvitation = this.store.createRecord('invitation', { email : email});
+
+          newInvitation.save();
+
+          this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
           this.set('emailAddress', '');
           this.set('message', '');
       }
