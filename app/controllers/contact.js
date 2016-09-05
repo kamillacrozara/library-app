1cc1d6f7c8fb5607c755d469349911e13ff89d07
Kamilla <holanda.kamilla@gmail.com>
2016-09-05 17:05:22 -0300

    Finishing lesson 2

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -17,7 +17,7 @@ export default Ember.Controller.extend({
     actions: {
       saveMessage(){
           alert(`Saving message of the following email address is in progress: ${this.get('emailAddress')}`);
-          this.set('responseMessage', `Thank you! We've just saved your message.`);
+          this.set('responseMessage', `We got your message and we’ll get in touch soon.`);
           this.set('emailAddress', '');
           this.set('message', '');
       }
