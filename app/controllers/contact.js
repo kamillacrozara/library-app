2b13ec9982f323017fc1efe382ffa187f91e1d61
Kamilla <holanda.kamilla@gmail.com>
2016-09-08 21:59:29 -0300

    Lesson 6

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -29,5 +29,6 @@ export default Ember.Controller.extend({
           });
 
       }
+  }
 
 });
--- app/router.js
+++ app/router.js
@@ -13,6 +13,7 @@ Router.map(function() {
     this.route('invitations');
     this.route('contact');
     this.route('contacts');
+    this.route('seeder');
   });
 
   this.route('libraries', function() {
